f2512e3f33309dc7580f48ac5aec161c1bfa90f4
Karel Veldkamp <karel@Karels-MBP.home>
2022-12-26 13:03:02 +0100

    added relpies with article urls

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ import tweepy
 import openai
 from newsapi.newsapi_client import NewsApiClient
 import datetime
+import string
+import time
+
+
+def string_cleaner(str):
+    """
+    Helper function that removes interpunction and converts to lowerase
+    :param string: a string
+    :return: a cleaned string
+    """
+    # convert to lowercase
+    result = str.lower()
+    # remove interpunction
+    result = result.translate(str.maketrans('', '', string.punctuation))
+    return result
 
 
 class OptiBot():
@@ -40,16 +55,18 @@ class OptiBot():
                                           sort_by='relevancy',
                                           page=1)['articles']
 
-        headlines = [a['title'] for a in articles][:n]
+        #headlines = [a['title'] for a in articles][:n]
 
-        return headlines
+        return articles[:n]
 
-    def generate_tweet(self, headlines):
+    def generate_tweet(self, articles):
         """
         Generate tweet using GPT-3
         :param headlines: list of news headlines
         :return: string to tweet to Twitter
         """
+
+        headlines = [a['title'] for a in articles]
         prompt = 'Write an optimistic and funny tweet mentioning at least one of these headlines: \n\n'
         prompt += "\n".join(headlines)
 
@@ -63,20 +80,50 @@ class OptiBot():
                 prompt=prompt,
                 temperature=0.8,
                 max_tokens=90,
-                n = 1
+                n=1
             )
             text = response['choices'][0]['text']
             nchar = len(text)
 
         return text
 
-    def tweet(self, text):
+    def find_mentioned_headlines(self, tweet, articles):
+        """
+                Generate tweet using GPT-3
+                :param headlines: list of news headlines
+                :return: string to tweet to Twitter
+                """
+        headlines = [a['title'] for a in articles]
+        prompt = 'below is a tweet and a list of headlines. ' \
+                 'which of the headlines are explicitly mentioned in the tweet? \n\n'
+        prompt += tweet
+        prompt += "\n".join(headlines)
+
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=prompt,
+            temperature=0.8,
+            max_tokens=90,
+            n=1
+        )
+
+        used_headlines = [string_cleaner(hl) for hl in response['choices'][0]['text'].splitlines()]
+
+        urls = []
+        for a in articles:
+            if string_cleaner(a['title']) in used_headlines:
+                urls.append(a['url'])
+
+        return urls
+
+    def tweet(self, text, reply_id=None):
         """
         posts a tweet to Twitter.
         :param text: text to post
+        :param reply_id: tweet id to reply to
         """
-        self.twitter_client.create_tweet(text=text)
-
+        response = self.twitter_client.create_tweet(text=text, in_reply_to_tweet_id=reply_id)
+        return response[0]['id']
 
 if __name__ == "__main__":
     with open("./credentials.yml", "r") as f:
@@ -85,7 +132,9 @@ if __name__ == "__main__":
     bot = OptiBot('https://en.wikinews.org/wiki/Main_Page', creds)
 
     headlines = bot.read_headlines(25)
-    print(headlines)
     generated_tweet = bot.generate_tweet(headlines)
-    print(generated_tweet)
-    bot.tweet(generated_tweet)
+    urls = bot.find_mentioned_headlines(generated_tweet, headlines)
+    id = bot.tweet(generated_tweet)
+    for url in urls:
+        time.sleep(10)
+        bot.tweet(url, id)
